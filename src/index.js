463eb9b2e3aa2d2c049566aaa73ed5ec1e16d771
Rohit Vishwakarma <143874936+Rohitvish04@users.noreply.github.com>
2025-06-18 18:14:23 +0000

    port

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ dotenv.config({
 
 connectDB()
 .then(()=>{
-app.listen(process.env.PORT|| 8000,()=>{
+app.listen(process.env.PORT || 10000,()=>{
   console.log(`server runing on port ${process.env.PORT}`)
 })
 
